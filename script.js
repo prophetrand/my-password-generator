d8ca34df09379d3a43484dd8ba0ea98508694885
Rand Hale <randluca@gmail.com>
2020-12-17 19:46:47 -0800

    declare charHolder empty array to hold each character added to the password through a forthcoming for loop

--- script.js
+++ script.js
@@ -6,21 +6,23 @@ var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 // This array has specialChars.length = 32 
 var specialChars =  [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
-console.log(specialChars.length);
-
-
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var charHolder = [];
 
   passwordText.value = password;
 
   // note to RAND: I will need a For loop that iterates through each index of the array/string(?) the number of times that the user inputs for the password length. Maybe have each randomly generated character populate an index of another array (with .push()), then run a .join() method (google this it might be called something else) to consolidate the array into one string again.
   function generatePassword(){
-    return "LEIAPOOPS"
+    var userLength = prompt("How many characters long do you want your password to be?");
+    
+    for (var i = 0; i < userLength; i++){
+      charHolder.push()
+    }
+
   }
 }
 
